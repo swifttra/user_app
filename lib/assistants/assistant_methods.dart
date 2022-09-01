1aaa3cf8fb3db3b91df91e98ff4113d5e0a13fba
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-09-01 08:40:05 +0100

    Payment

--- lib/assistants/assistant_methods.dart
+++ lib/assistants/assistant_methods.dart
@@ -85,11 +85,11 @@ class AssistantMethods {
   static double calculateFareAmountFromOriginToDestination(
       DirectionDetailsInfo directionDetailsInfo) {
     double timeTraveledFareAmountPerMinute =
-        (directionDetailsInfo.duration_value! / 60) * 0.1;
+        (directionDetailsInfo.duration_value! / 60) * 9;
     double distanceTraveledFareAmountPerKilometer =
-        (directionDetailsInfo.duration_value! / 1000) * 0.1;
+        (directionDetailsInfo.duration_value! / 1000) * 65;
 
-    //USD
+    //NGN
     double totalFareAmount = timeTraveledFareAmountPerMinute +
         distanceTraveledFareAmountPerKilometer;
 
